532d1b2a55b81dd3854164c6a2c7e5abd271d826
MatheusSansonowicz <128191037+MatheusSansonowicz@users.noreply.github.com>
2023-11-13 18:04:28 -0300

    Update GerarAmigo.h

--- GerarAmigo.h
+++ GerarAmigo.h
@@ -12,7 +12,16 @@ void GerarAmigo(){
     }
     else
     {
-        
+         // Embaralhe a lista de amigos
+        random_shuffle(listaAmigos.begin(), listaAmigos.end());
+
+        // Atribua cada amigo secreto
+        for (size_t i = 0; i < listaAmigos.size(); ++i) {
+            // Se for o último da lista, o amigo secreto é o primeiro
+            size_t indexAmigoSecreto = (i == listaAmigos.size() - 1) ? 0 : i + 1;
+
+            listaAmigos[i].amigoSecreto = listaAmigos[indexAmigoSecreto].nome;
+    }
     }
     
 }
